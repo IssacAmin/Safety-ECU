847fee039479c59c63fd321d92cc9ffaa5ddcdab
OmarT-Y <omartamerz9@gmail.com>
2025-05-09 04:26:22 +0300

    added openBSW as sub module and created s32 project for testing

---
+++ openBSW_ECU/S32_WS/openBSW/openBSW_Z4_0/src/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * main implementation: use this 'C++' sample to create your own application
+ *
+ */
+
+#include "derivative.h" /* include peripheral declarations */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void xcptn_xmpl(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+class counterclass
+{
+private:
+  	int m_counter;
+public:  
+	counterclass(void) 
+	{
+		m_counter = 0;
+	};
+	void increment(void) 
+	{
+		m_counter++;
+	};  	
+};
+extern void app_main();
+int main()
+{
+	counterclass myccounter;
+
+    xcptn_xmpl ();              /* Configure and Enable Interrupts */
+
+    /* Loop forever */
+    app_main();
+	for(;;) {	
+			myccounter.increment();
+		}
+}
